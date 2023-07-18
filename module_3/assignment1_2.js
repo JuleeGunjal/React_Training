f1d5108f06f4cde8507237c6699d461c68283d7f
Julee <julee.gunjal@joshsoftware.com>
2023-07-18 20:03:49 +0530

    module 3 assignment

---
+++ module_3/assignment1_2.js
@@ -0,0 +1,14 @@
+let age = 25;
+
+switch (true){
+  case (age <= 12):
+    console.log("child");
+    break;
+  case (18 <= age >= 13):
+    console.log("teen");
+    break;
+  case (age > 18):
+    console.log("adult");
+    break;
+}
+ // Gives o/p as "adult"

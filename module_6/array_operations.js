60dd9f0c2d4bc0ad5984f24794921bae2246e0ab
JuleeGunjal <118346732+JuleeGunjal@users.noreply.github.com>
2023-08-08 12:19:46 +0530

    Module 6 assignments (#6)
    * Module 6 assignments
    
    * Chnages done as per comments to use destructure

---
+++ module_6/array_operations.js
@@ -0,0 +1,210 @@
+let array = [
+  {
+    id: 1,
+    first_name: "Nicki",
+    email: "ncrozier0@squarespace.com",
+    date_of_birth: "2009/05/09",
+  },
+  {
+    id: 2,
+    first_name: "Raychel",
+    email: "rmcgrady1@cpanel.net",
+    date_of_birth: "1996/11/05",
+  },
+  {
+    id: 3,
+    first_name: "Demetris",
+    email: "dkilshall2@elpais.com",
+    date_of_birth: "2018/12/31",
+  },
+  {
+    id: 4,
+    first_name: "Amata",
+    email: "abraiden3@canalblog.com",
+    date_of_birth: "2012/05/23",
+  },
+  {
+    id: 5,
+    first_name: "Venita",
+    email: "vheap4@clickbank.net",
+    date_of_birth: "2020/10/04",
+  },
+  {
+    id: 6,
+    first_name: "Fairfax",
+    email: "fcrichton5@merriam-webster.com",
+    date_of_birth: "2009/12/23",
+  },
+  {
+    id: 7,
+    first_name: "Kathleen",
+    email: "kvasyukhnov6@devhub.com",
+    date_of_birth: "2010/12/20",
+  },
+  {
+    id: 8,
+    first_name: "Sam",
+    email: "scorck7@sitemeter.com",
+    date_of_birth: "2020/08/30",
+  },
+  {
+    id: 9,
+    first_name: "Virgilio",
+    email: "vferandez8@e-recht24.de",
+    date_of_birth: "2000/09/07",
+  },
+  {
+    id: 10,
+    first_name: "Townie",
+    email: "tpetyt9@upenn.edu",
+    date_of_birth: "2018/09/01",
+  },
+];
+
+// Write a function filterByName that accepts a string as a parameter
+// and returns an array with only those objects where the first_name field includes that string.
+
+let filterByName = (nameParam) => {
+  return array.filter((item) => item.first_name.includes(nameParam));
+};
+
+let requiredObject = filterByName("ta");
+console.log(requiredObject);
+// OUTPUT => got the array containing ta in the first name field
+// [
+//   {
+//     id: 4,
+//     first_name: 'Amata',
+//     email: 'abraiden3@canalblog.com',
+//     date_of_birth: '2012/05/23'
+//   },
+//   {
+//     id: 5,
+//     first_name: 'Venita',
+//     email: 'vheap4@clickbank.net',
+//     date_of_birth: '2020/10/04'
+//   }
+// ]
+
+// Use Array.map to return an array of all the email fields.
+
+let getEmails = () => {
+  let emailArray = array.map((item) => {
+  const {id , email} = item;
+  return id + "_email: " + email;
+});
+  return emailArray;
+};
+
+console.log(getEmails());
+// OUTPUT:
+// [
+//   'ncrozier0@squarespace.com',
+//   'rmcgrady1@cpanel.net',
+//   'dkilshall2@elpais.com',
+//   'abraiden3@canalblog.com',
+//   'vheap4@clickbank.net',
+//   'fcrichton5@merriam-webster.com',
+//   'kvasyukhnov6@devhub.com',
+//   'scorck7@sitemeter.com',
+//   'vferandez8@e-recht24.de',
+//   'tpetyt9@upenn.edu'
+// ]
+
+// Use Array.sort to return the array sorted in descending order by date_of_birth.
+
+let sortByBirthDateDesc = () => {
+  let sortedArray = array.sort((a, b) => sortFunction(a, b));
+  return sortedArray;
+};
+
+let sortFunction = (a, b) => {
+  const { date_of_birth: firstDob } = a;
+  const { date_of_birth: secondDob } = b;
+  if (firstDob > secondDob) return 1;
+  else if (firstDob === secondDob) return 0;
+  else return -1;
+};
+
+console.log(sortByBirthDateDesc());
+// OutPut:
+// [
+//   {
+//     id: 2,
+//     first_name: 'Raychel',
+//     email: 'rmcgrady1@cpanel.net',
+//     date_of_birth: '1996/11/05'
+//   },
+//   {
+//     id: 9,
+//     first_name: 'Virgilio',
+//     email: 'vferandez8@e-recht24.de',
+//     date_of_birth: '2000/09/07'
+//   },
+//   {
+//     id: 1,
+//     first_name: 'Nicki',
+//     email: 'ncrozier0@squarespace.com',
+//     date_of_birth: '2009/05/09'
+//   },
+//   {
+//     id: 6,
+//     first_name: 'Fairfax',
+//     email: 'fcrichton5@merriam-webster.com',
+//     date_of_birth: '2009/12/23'
+//   },
+//   {
+//     id: 7,
+//     first_name: 'Kathleen',
+//     email: 'kvasyukhnov6@devhub.com',
+//     date_of_birth: '2010/12/20'
+//   },
+//   {
+//     id: 4,
+//     first_name: 'Amata',
+//     email: 'abraiden3@canalblog.com',
+//     date_of_birth: '2012/05/23'
+//   },
+//   {
+//     id: 10,
+//     first_name: 'Townie',
+//     email: 'tpetyt9@upenn.edu',
+//     date_of_birth: '2018/09/01'
+//   },
+//   {
+//     id: 3,
+//     first_name: 'Demetris',
+//     email: 'dkilshall2@elpais.com',
+//     date_of_birth: '2018/12/31'
+//   },
+//   {
+//     id: 8,
+//     first_name: 'Sam',
+//     email: 'scorck7@sitemeter.com',
+//     date_of_birth: '2020/08/30'
+//   },
+//   {
+//     id: 5,
+//     first_name: 'Venita',
+//     email: 'vheap4@clickbank.net',
+//     date_of_birth: '2020/10/04'
+//   }
+// ]
+
+// Write a function getById that accepts a number as a parameter and returns the
+// object where the id is equal to that number.
+
+let getById = (paramId) => {
+  return array.filter((item) => item.id === paramId);
+};
+
+console.log(getById(9));
+// output
+// [
+//   {
+//     id: 9,
+//     first_name: 'Virgilio',
+//     email: 'vferandez8@e-recht24.de',
+//     date_of_birth: '2000/09/07'
+//   }
+// ]
---
+++ module_6/manipulating_non_manipulating.js
@@ -0,0 +1,138 @@
+// What makes a method mutating or non mutating in Javascript?
+
+// => Mutating methods are array methods that modify the original array,
+// while non-mutating methods return a new array without modifying the original.
+
+
+// Find out whether each of the following methods are mutating or non-mutating. How can you verify this?
+let array = [2, 6, 9, 4, 12, 1, 14];
+console.log(array);
+// Original array:
+// [
+//   2, 6,  9, 4,
+//  12, 1, 14
+// ]
+
+// 1.push
+array.push (10);
+console.log(array);
+// Array after the push operation:
+// [
+//   2, 6,  9,  4,
+//  12, 1, 14, 10
+// ]
+
+// CONCLUSION:
+// push() - Adds one or more elements to the end of an array and returns manipulated array
+// => push is a manipulating method
+
+// 2.pop
+
+// Array before pop operation:
+// [
+//   2, 6,  9,  4,
+//  12, 1, 14, 10
+// ]
+array.pop();
+console.log(array);
+// Array after the pop operation:
+// [
+//   2, 6,  9,  4,
+//  12, 1, 14
+// ]
+
+// CONCLUSION : 
+// pop() - Removes the last element from an array and returns manipulated array
+// i.e. pop() is also the manipulating method
+
+// 3.filter
+
+let filteredArray = array.filter(item => item > 10);
+console.log("Filtered array: ", filteredArray); // OutPut : Filtered array:  [ 12, 14 ]
+console.log("Array after filter manipulation: ", array);
+// OutPut:
+// Array after filter manipulation:  [
+//   2, 6,  9, 4,
+//  12, 1, 14
+// ]
+
+// CONCLUSION:
+// filter: It returns the new array of elements which satisfies the given filter condition
+// It does not manipulate the original array
+// Thus the filter method is non manipulating method
+
+
+// 4.find
+
+let ele = array.find(item => item > 10);
+console.log(ele); // 12
+console.log("Array after find manipulation: ", array);
+// OutPut:
+// Array after find manipulation:  [
+//   2, 6,  9, 4,
+//  12, 1, 14
+// ]
+
+// CONCLUSION:
+// find() => find and return the first element from an array which satisfies the condition without manipulating the original array
+// => find() is a non-manipulating method
+
+// 5.sort
+console.log("Array before sort done: ", array);
+//output:
+// Array before sort done:  [
+//   2, 6,  9, 4,
+//  12, 1, 14
+// ]
+let sortedArray = array.sort((a, b) =>  a - b);
+console.log("Sorted array: ", sortedArray);
+// Output
+// Sorted array:  [
+//   1,  2,  4, 6,
+//   9, 12, 14
+// ]
+console.log("Array after sort done: ", array);
+// Array after sort done:  [
+//   1,  2,  4, 6,
+//   9, 12, 14
+// ]
+// CONCLUSION:
+// sort() => sorts the original array
+// => sort() is a manipulating method
+
+// 6. map
+
+console.log("Array before map done: ", array);
+//output:
+// Array before map done:  [
+//   1,  2,  4, 6,
+//   9, 12, 14
+// ]
+
+let mappedArray = array.map(item => item * 2);
+
+console.log("Mapped array: ", mappedArray);
+// OutPut:
+// Mapped array:  [
+//   2,  4,  8, 12,
+//  18, 24, 28
+// ]
+
+console.log("Original Array After map operation: ", array);
+// OutPut:
+// Original Array After map operation:  [
+//   1,  2,  4, 6,
+//   9, 12, 14
+// ]
+
+// CONCLUSION:
+// map(): It returns the new array with elements manipulating through the map function 
+// It does not manipulate the original array
+// Thus the map method is non manipulating method
+
+// Manipulating methods:
+// push(), pop(), sort();
+// Non manipulating methods:
+// filter(), find(), map();
+
+
